1d0b265f8061eb6a25eb30497877165bd71988b9
Mauricio Moraga <mauricio.moraga@alumnos.uv.cl>
2024-12-10 09:10:20 -0300

    feat:(query added)

---
+++ src/db/query.sql
@@ -0,0 +1,10 @@
+-- Active: 1733169994794@@127.0.0.1@5434@test_db@public
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL
+);
